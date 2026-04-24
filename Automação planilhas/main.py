9b4dbf5bf8557a4404601b517a3cf98fa5b95140
Júlio <juliojoik@gmail.com>
2026-04-24 11:28:31 -0300

    Update main.py
    Adição de alguns comentários, ajudando o usuário a entender as funções.

--- Automação planilhas/main.py
+++ Automação planilhas/main.py
@@ -13,14 +13,14 @@ caminho_planilha = filedialog.askopenfilename(title="Selecione a planilha para p
 dataFrame = pd.read_excel(caminho_planilha, header=2, sheet_name='Concluídos(Aprovados)')
 df = pd.DataFrame(dataFrame)
 
-padronizadorPlanilha.novasColunas(df)
-padronizadorPlanilha.reembolsavel(df)
-padronizadorPlanilha.mapCC(df)
-padronizadorPlanilha.mapCorrespondente(df)
+padronizadorPlanilha.novasColunas(df) #Essa função irá adicionar 6 novas colunas das quais estão nomeadas com o padrão da empresa, mas sinta-se a vontade para mudar o nome para o seu padrão.
+padronizadorPlanilha.reembolsavel(df) #Essa função utiliza um metodo do Pandas chamado .loc que é util para verificar uma célula já existente, e preencher uma celula de acordo com condição fornecida.
+padronizadorPlanilha.mapCC(df) #Essa função utiliza uma função do Pandas chamada .map() que ajuda a localizar e preencher informações em uma celula de acordo com um dicionário.
+padronizadorPlanilha.mapCorrespondente(df) #Essa função também utiliza a função do Pandas .map() que irá preencher uma nova coluna criada atráves do padronizadorPlanilha.novasColunas()
 
-df.to_excel("PlanilhaPadronizada.xlsx")
+df.to_excel("PlanilhaPadronizada.xlsx") #Aqui será realizado o download da planilha já com as alterações feitas, porém ainda não é a planilha final, pois a biblioteca pandas imprime as datas no formato de EUA.
 print(df.dtypes)
-padronizadorPlanilha.formatacaoDatas("PlanilhaPadronizada.xlsx")
+padronizadorPlanilha.formatacaoDatas("PlanilhaPadronizada.xlsx") #O Download da planilha final terá o nome de PlanilhaPadronizada.xlsx, onde todas as formatações estarão completas e com datas no formato brasileiro.
 
 
 
